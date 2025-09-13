c4126479d49e41df74f8a7b32cf1b10b23064756
Zakhele <prince.feela2905@gmail.com>
2025-09-13 20:28:50 +0200

    .

---
+++ app/index.tsx
@@ -0,0 +1,10 @@
+// Example for Home Screen
+import { View, Text, Image } from 'react-native';
+
+export default function HomeScreen() {
+  return (
+    <View>
+      <Text>Home Screen</Text>
+    </View>
+  );
+}
